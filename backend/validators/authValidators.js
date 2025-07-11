eb3d876bdb276fcb9a62b5311f23357452d6a0b1
mencychristian012 <70622045+mencychristian012@users.noreply.github.com>
2025-07-12 02:16:31 +0530

    Update authValidators.js

--- backend/validators/authValidators.js
+++ backend/validators/authValidators.js
@@ -1,19 +1,19 @@
 const { body, validationResult } = require("express-validator");
 
-export const validateRegister = [
+const validateRegister = [
   body("name")
     .isString()
     .withMessage("Name must be a string")
     .trim()
     .notEmpty()
     .withMessage("Name is required"),
-  
+
   body("email").isEmail().withMessage("Enter a valid email").normalizeEmail(),
-  
+
   body("password")
     .isLength({ min: 6 })
     .withMessage("Password must be at least 6 characters"),
-  
+
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -29,11 +29,11 @@ export const validateRegister = [
   },
 ];
 
-export const validateLogin = [
+const validateLogin = [
   body("email").isEmail().withMessage("Enter a valid email").normalizeEmail(),
-  
+
   body("password").notEmpty().withMessage("Password is required"),
-  
+
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -48,3 +48,8 @@ export const validateLogin = [
     next();
   },
 ];
+
+module.exports = {
+  validateRegister,
+  validateLogin,
+};
