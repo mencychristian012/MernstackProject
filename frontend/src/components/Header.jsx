bce8dad4cb27c1fa58a740c4a3a36b63859e9f6e
mencychristian012 <70622045+mencychristian012@users.noreply.github.com>
2025-07-12 04:53:42 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/home">Home</Nav.Link>
                         <Nav.Link href="/products">Products</Nav.Link>
                         <Nav.Link href="/contact">Contacts</Nav.Link>
                     </Nav>
